3ff474f12196d6fff4007aaab9750dbfd48e8398
devops4sure <akshaymandre111@gmail.com>
2024-08-20 15:43:14 +0530

    provisioner

---
+++ Provisioners/instance.tf
@@ -0,0 +1,34 @@
+resource "aws_key_pair" "sample-key" {
+    key_name   = "crispykey"
+    public_key = file("samplekey.pub") # generate public-key & private-key  i.e [ssh-keygen],name=samplekey
+  }
+  
+  resource "aws_instance" "sample-inst" {
+    ami                    = var.AMIS[var.REGION]
+    instance_type          = var.INSTANCE_TYPE
+    availability_zone      = var.ZONE1
+    key_name               = aws_key_pair.sample-key.key_name
+    vpc_security_group_ids = ["sg-063ea78c79c5ba038"]
+    tags = {
+      Name    = "Sample-Instance"
+      Project = "Sample"
+    }
+  
+    provisioner "file" {
+      source      = "web.sh"
+      destination = "/tmp/web.sh"
+    }
+  
+    provisioner "remote-exec" {
+      inline = [
+        "chmod u+x /tmp/web.sh",
+        "sudo /tmp/web.sh"
+      ]
+    }
+  
+    connection {
+      user        = var.USER
+      private_key = file("samplekey")   # generate public-key & private-key  i.e [ssh-keygen],name=samplekey
+      host        = self.public_ip
+    }
+  }
---
+++ Provisioners/vars.tf
@@ -0,0 +1,23 @@
+variable "REGION" {
+    default = "us-west-2"
+  }
+  
+  variable "ZONE1" {
+    default = "us-west-2a"
+  }
+  
+  variable "INSTANCE_TYPE" {
+    default = "t2.micro"
+  }
+  
+  variable "AMIS" {
+    type = map(any)
+    default = {
+      us-west-2 = "ami-094cc0ced7b91fcf0"
+      us-east-2 = "ami-0862be96e41dcbf74"
+    }
+  }
+  
+  variable "USER" {
+    default = "ec2-user"
+  }
